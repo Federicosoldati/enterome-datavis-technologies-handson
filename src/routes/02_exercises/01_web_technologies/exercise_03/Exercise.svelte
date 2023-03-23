2400b8f30458c39ed3cfac9786bd8b19518524aa
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-03-23 11:13:11 +0100

    Ex4

---
+++ src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
@@ -0,0 +1,7 @@
+<div>"The best visual channels ordered by Mackinlay's ranking:"
+</div>
+<ol>
+    <li>Angle</li>
+    <li>Length</li>
+    <li>Position</li>
+  </ol>
--- src/routes/02_exercises/01_web_technologies/exercise_04/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_04/Exercise.svelte
@@ -1,1 +1,1 @@
-Marks are basic geometric elements that depict items and links.
+<em>Marks</em> are <s>basic</s> geometric <b>elements</b> that depict items and links.
