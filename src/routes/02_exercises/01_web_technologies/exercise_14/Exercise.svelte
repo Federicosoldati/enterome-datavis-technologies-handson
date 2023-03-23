3bef7efb2fee218d5f27d1d7e862c5e740e8fd26
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-03-23 23:13:53 +0100

    Ex14

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -1,16 +1,31 @@
 <script>
   let current = "";
+  let lastClickedButton = null;
+  
+  const assignClass = (button) => {
+    if (lastClickedButton !== null) {
+      lastClickedButton.classList.remove("bg-color");
+    }
+    button.classList.add("bg-color");
+    lastClickedButton = button;
+  }
 </script>
 
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => (current = "messi")}>
+<button class="btn btn-secondary" on:click={() => {current = "messi"; assignClass($event.target);}}>
   Messi
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "ronaldo")}>
+<button class="btn btn-secondary" on:click={() => {current = "ronaldo"; assignClass($event.target);}}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "neymar")}>
+<button class="btn btn-secondary" on:click={() => {current = "neymar"; assignClass($event.target);}}>
   Neymar
 </button>
+
+<style>
+  .bg-color {
+    background-color: #00ff00;
+  }
+</style>
