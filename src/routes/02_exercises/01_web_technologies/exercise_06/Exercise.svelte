05298b2298baba076760137aa87f2240a9e4f12b
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-03-23 11:21:39 +0100

    Ex6

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,13 @@
-I really like data visualisation!
+<div>I <span style="color:grey">really </span> like data visualisation!</div>
+
+<style>
+  div{
+    color: steelblue;
+  }
+  div{
+  font-family: "Times New Roman", Times, serif;
+  font-size: large;
+  }
+
+</style>
+    
