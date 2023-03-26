2cef3e3025ea29ac9fb48a429c25976d3b9c564a
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-03-26 12:30:05 +0200

    Ex14

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -1,31 +1,21 @@
 <script>
   let current = "";
-  let lastClickedButton = null;
-  
-  const assignClass = (button) => {
-    if (lastClickedButton !== null) {
-      lastClickedButton.classList.remove("bg-color");
-    }
-    button.classList.add("bg-color");
-    lastClickedButton = button;
-  }
 </script>
+<style>
+  .bluecolor {
+background-color: lightsalmon;
+  }
+</style>
 
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => {current = "messi"; assignClass($event.target);}}>
+<button class="btn btn-secondary" class:bluecolor={current=="messi"} on:click={() => (current = "messi")}>
   Messi
 </button>
 
-<button class="btn btn-secondary" on:click={() => {current = "ronaldo"; assignClass($event.target);}}>
+<button class="btn btn-secondary" class:bluecolor={current=="ronaldo"} on:click={() => (current = "ronaldo")}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => {current = "neymar"; assignClass($event.target);}}>
+<button class="btn btn-secondary" class:bluecolor={current=="neymar"} on:click={() => (current = "neymar")}>
   Neymar
 </button>
-
-<style>
-  .bg-color {
-    background-color: #00ff00;
-  }
-</style>
