de4d781484f8469fd68b55ff3c425720c02f2026
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-04-04 17:30:13 +0200

    Ex18(not working)

--- src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
@@ -1,18 +1,80 @@
 <script>
+  // Dimensions
+  const width = 800;
+  const height = 100;
+  const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  // Scales
+  import {extent} from 'd3-array';
+  import {select} from 'd3-selection';
+  import { scaleLog } from 'd3-scale';
+  const scale = scaleLog() 
+    .domain([1, 10])
+    .range([0, innerWidth])
+
+  import { axisBottom } from 'd3-axis';
+  const xAxis = axisBottom()
+    .scale(scale);  
+
+  // Array
+  const values = [2, 4, 6, 7, 9];
+
+  function myFunction(handle){
+    console.log('Action called');
+  }
+
+  console.log('Script finished');
+</script>
+
+<svg viewBox="0 0 {width} {height}">
+  <g transform="translate({margin.left},{margin.top})">
+    {#each values as value}
+      <circle cx={scale(value)} cy={innerHeight/2} r=10 />
+    {/each}
+  </g>
+  <script>
     // Dimensions
     const width = 800;
     const height = 100;
-    const margin = { top: 5, right: 5, bottom: 25, left: 5 };
+    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
   
+    // Scales
+    import {extent} from 'd3-array';
+    import {select} from 'd3-selection';
+    import { scaleLog } from 'd3-scale';
+    const scale = scaleLog() 
+      .domain([1, 10])
+      .range([0, innerWidth])
+  
+    import { axisBottom } from 'd3-axis';
+    const xAxis = axisBottom()
+      .scale(scale);  
+  
     // Array
     const values = [2, 4, 6, 7, 9];
+  
+    function myFunction(handle){
+      console.log('Action called');
+    }
+  
+    console.log('Script finished');
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each values as value}
+        <circle cx={scale(value)} cy={innerHeight/2} r=10 />
+      {/each}
     </g>
-  </svg>
+
+    <g use:myFunction>
+      <xAxis(select())>
+    </g> 
   
+  </svg>
+
+</svg>
