d62ce6a60b7676128b051fd6a74dea3a68ed3be0
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-04-04 16:49:15 +0200

    Ex17

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -5,6 +5,12 @@
     const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
+
+    // Scales
+    import { scaleLog } from 'd3-scale';
+    const scale = scaleLog() 
+      .domain([1, 10])
+      .range([0, innerWidth])
   
     // Array
     const values = [2, 4, 6, 7, 9];
@@ -12,7 +18,9 @@
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each values as value}
+        <circle cx={scale(value)} cy={innerHeight/2} r=10 />
+      {/each}
     </g>
   </svg>
   
