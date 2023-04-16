32ff827bf9d97dc72ceeb11d9249a179c84c84bc
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-04-16 17:33:45 +0200

    Ex18

--- src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
@@ -2,79 +2,39 @@
   // Dimensions
   const width = 800;
   const height = 100;
-  const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+  const margin = { top: 5, right: 5, bottom: 20, left: 5 };
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
   // Scales
-  import {extent} from 'd3-array';
-  import {select} from 'd3-selection';
   import { scaleLog } from 'd3-scale';
-  const scale = scaleLog() 
-    .domain([1, 10])
-    .range([0, innerWidth])
-
   import { axisBottom } from 'd3-axis';
-  const xAxis = axisBottom()
-    .scale(scale);  
+  import { select } from 'd3-selection';
 
-  // Array
+  const xScale = scaleLog()
+    .domain([1, 10])
+    .range([0, innerWidth]);
+
   const values = [2, 4, 6, 7, 9];
 
-  function myFunction(handle){
-    console.log('Action called');
-  }
+  function createXAxis(handle) {
+    const xAxis = axisBottom(xScale);
+    select(handle).call(xAxis);
 
-  console.log('Script finished');
+    select(handle)
+      .append("text")
+      .attr("x", innerWidth / 2)
+      .attr("y", margin.bottom - 5)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "middle")
+      .text("X Axis");
+  }
 </script>
 
-<svg viewBox="0 0 {width} {height}">
-  <g transform="translate({margin.left},{margin.top})">
-    {#each values as value}
-      <circle cx={scale(value)} cy={innerHeight/2} r=10 />
-    {/each}
-  </g>
-  <script>
-    // Dimensions
-    const width = 800;
-    const height = 100;
-    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-  
-    // Scales
-    import {extent} from 'd3-array';
-    import {select} from 'd3-selection';
-    import { scaleLog } from 'd3-scale';
-    const scale = scaleLog() 
-      .domain([1, 10])
-      .range([0, innerWidth])
-  
-    import { axisBottom } from 'd3-axis';
-    const xAxis = axisBottom()
-      .scale(scale);  
-  
-    // Array
-    const values = [2, 4, 6, 7, 9];
-  
-    function myFunction(handle){
-      console.log('Action called');
-    }
-  
-    console.log('Script finished');
-  </script>
-  
-  <svg viewBox="0 0 {width} {height}">
-    <g transform="translate({margin.left},{margin.top})">
-      {#each values as value}
-        <circle cx={scale(value)} cy={innerHeight/2} r=10 />
-      {/each}
-    </g>
-
-    <g use:myFunction>
-      <xAxis(select())>
-    </g> 
-  
-  </svg>
-
+<svg viewBox={`0 0 ${width} ${height}`}>
+  <g transform={`translate(${margin.left},${margin.top})`} />
+  <g transform={`translate(${margin.left},${innerHeight + margin.top})`} use:createXAxis />
+  {#each values as value}
+    <circle cx={xScale(value)} cy={innerHeight/2} r=10 />
+  {/each}
 </svg>
